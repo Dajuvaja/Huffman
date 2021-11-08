9f7767683a28e01c184d9ecdf0d0c11c4573babe
Daniel Vargas <danieljulian2848@gmail.com>
2021-11-07 22:20:58 -0500

    Adding the project

---
+++ HuffmanCera/src/huffmancera/HuffmanCera.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huffmancera;
+
+import GUI.Ventana;
+
+/**
+ *
+ * @author LAO BARRERA
+ */
+public class HuffmanCera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //HuffmanLib huf = new HuffmanLib();
+        /*System.out.println(huf.frecu);
+        System.out.println(huf.Tablas.get(0).cars);
+        System.out.println(huf.Tablas.get(0).Colas.get(6));
+        System.out.println(huf.Tablas.get(0).Colas.get(12));
+        System.out.println(huf.Tablas.get(0));
+        huf.calcularT();*/
+        //huf.encriptar();
+        //System.out.println(huf);
+        //System.out.println(huf.cripString());
+        //System.out.println(huf.menString());
+        //Ventana v = new Ventana();
+        Ventana.main(args);
+    }
+    
+}
---
+++ src/Huffman/Huffman.java
@@ -0,0 +1,11 @@
+package Huffman;
+
+import GUI.Ventana;
+
+public class Huffman {
+
+    public static void main(String[] args) {;
+        Ventana.main(args);        
+    }
+    
+}
